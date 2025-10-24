3e50574171a7a2278ef49869c290678ba9361449
AG2048 <45613281+AG2048@users.noreply.github.com>
2025-10-24 00:25:27 -0400

    Rename top module to tt_um_ag2048_calculator
    Updated the top module name from tt_um_tiny_calculator to tt_um_ag2048_calculator in info.yaml, src/project.v, and test/tb.v to ensure consistency and uniqueness as required.

--- test/tb.v
+++ test/tb.v
@@ -27,7 +27,7 @@ module tb ();
   wire VGND = 1'b0;
 `endif
 
-  tt_um_tiny_calculator user_project (
+  tt_um_ag2048_calculator user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
