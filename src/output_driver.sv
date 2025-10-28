b5cb2d92a38ab416fa0e86ef1ab2888507490060
AG2048 <45613281+AG2048@users.noreply.github.com>
2025-10-27 23:55:15 -0400

    Add module documentation and TODOs to output_driver.sv
    Added detailed comments describing the output_driver module's intended functionality, including data conversion for 7-segment display and handshake protocol. Included several TODOs for future implementation details and improvements.

--- src/output_driver.sv
+++ src/output_driver.sv
@@ -22,7 +22,24 @@ module output_driver #(
     output logic                  o_sr_clk,
     output logic                  o_sr_latch
 );
-// Temp assign all input to 0, tie all output to & _unused
+  /*
+    Output driver module that:
+      takes in a DATA_WIDTH data input, 
+      converts it to 7-seg display format,
+      output via shift register interface (o_sr_data, o_sr_clk, o_sr_latch).
+
+    Input follows a valid-ready handshake protocol.
+    When (i_valid && o_ready), input data is latched along with i_2s_comp bit indicating if data is in 2's complement format.
+      (value is negative if i_2s_comp is high and MSB of i_data is 1)
+    Data is then converted to HEX format for 7-seg display, 4-bit per digit.
+      (Convert lowest 4 bits, send result out, shift right by 4 bits, repeat for NUM_7_SEG_DISPLAYS times)
+      (TODO: Consider not illuminating leading zeros by checking if remaining bits are 0)
+      (TODO: When data is negative the most significant digit should show '-' sign)
+      (TODO: When data is negative, the conversion may be different)
+      (TODO: Must flush the shift register with EMPTY before sending new data / or we can send a reset signal to the shift register first)
+  */
+
+// TODO: remove these: Temp assign all input to 0, tie all output to & _unused
 assign {o_done, o_sr_data, o_sr_clk, o_sr_latch, o_ready} = '0;
 logic _unused = &{clk, rst_n, i_data, i_2s_comp, i_valid};
 
