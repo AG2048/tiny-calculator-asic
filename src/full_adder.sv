5fa986c8873951b6b766631cdd93306d9260b94f
AG2048 <45613281+AG2048@users.noreply.github.com>
2025-10-29 01:52:16 -0400

    Adding a genvar comment to remove a warning

--- src/full_adder.sv
+++ src/full_adder.sv
@@ -12,7 +12,7 @@ module full_adder #(
   assign carry_out = carry[DATA_WIDTH];
 
   assign carry[0] = carry_in;
-  for (genvar i = 0; i < DATA_WIDTH; i++) begin
+  for (genvar i = 0; i < DATA_WIDTH; i++) begin : gen_full_adder_bits
     assign sum[i]   = a[i] ^ b[i] ^ carry[i];
     assign carry[i+1] = (a[i] & b[i]) | (carry[i] & (a[i] ^ b[i]));
   end
