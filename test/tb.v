9d17e886f599d69062132bbd105bb44cd28f9579
AG2048 <45613281+AG2048@users.noreply.github.com>
2025-11-04 23:07:53 -0500

    Add signal wiring and pin assignments to testbench
    Introduced internal wire declarations and assigned uio and uo pins for state display and control signals in the testbench. This improves clarity and enables more comprehensive testing of the calculator module by mapping input and output signals explicitly.

--- test/tb.v
+++ test/tb.v
@@ -27,6 +27,62 @@ module tb ();
   wire VGND = 1'b0;
 `endif
 
+  // Wire for testing signals
+  // Internal signal declarations
+  wire [3:0] o_word_lines;
+  wire [3:0] i_bit_lines;
+
+  wire [4:0] input_value;
+
+  wire i_add_pin;
+  wire i_sub_pin;
+  wire i_mul_pin;
+  wire i_div_pin;
+  wire i_ac_pin;
+  wire i_eq_pin;
+  wire i_neg_pin;
+
+  wire i_2s_comp_mode_pin;
+
+  wire                  o_add_state_display;
+  wire                  o_sub_state_display;
+  wire                  o_mul_state_display;
+  wire                  o_div_state_display;
+
+  wire                  o_sr_data;
+  wire                  o_sr_clk;
+  wire                  o_sr_latch;
+  wire                  o_sr_oe_n;
+
+  // Assign uio pins direction and unused pins
+  assign uio_oe             = 8'b00111100;          // uio[2:5] are outputs, others are inputs
+  assign uio_out[1:0]       = 2'b00;                // Unused output pins
+  assign uio_out[2]         = o_add_state_display;
+  assign uio_out[3]         = o_sub_state_display;
+  assign uio_out[4]         = o_mul_state_display;
+  assign uio_out[5]         = o_div_state_display;
+  assign uio_out[7:6]       = 2'b00;                // Unused output pins
+
+  assign i_ac_pin           = uio_in[0];
+  assign i_eq_pin           = uio_in[1];
+  assign i_2s_comp_mode_pin = uio_in[6];
+  assign i_neg_pin          = uio_in[7];
+  wire   _unused            = &{uio_in[5:2], ena, 1'b0}; // Prevent unused input warnings
+
+  // Assign signals to Output Pins
+  assign uo_out[3:0] = o_word_lines;
+  assign uo_out[4]   = o_sr_data;
+  assign uo_out[5]   = o_sr_clk;
+  assign uo_out[6]   = o_sr_latch;
+  assign uo_out[7]   = o_sr_oe_n;
+  
+  // Assign Input Pins to signals
+  assign i_bit_lines        = ui_in[3:0];
+  assign i_add_pin          = ui_in[4];
+  assign i_sub_pin          = ui_in[5];
+  assign i_mul_pin          = ui_in[6];
+  assign i_div_pin          = ui_in[7];
+
   tt_um_ag2048_calculator user_project (
 
       // Include power ports for the Gate Level test:
